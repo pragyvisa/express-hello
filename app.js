a0414930eb079d547182da82b412ccb079b3b6bc
Gautham Pai <buzypi@gmail.com>
2019-01-03 11:08:26 +0530

    Checking if webhook trigger is fine

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello!'))
 
-app.get('/anotherhello', (req, res) => res.send('Another Hello!'))
+app.get('/anotherhello', (req, res) => res.send('Another Hello!!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
