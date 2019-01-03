ae96e05232480a70454fb889ce5c2b2e1f7ffe1d
Gautham Pai <buzypi@gmail.com>
2019-01-03 10:58:34 +0530

    Checking if webhooks work

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/anotherhello', (req, res) => res.send('Another Hello!'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
