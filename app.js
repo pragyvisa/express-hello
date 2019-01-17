0bea511c6de45d6e23c186fb5b5ecaca217a3691
Gautham Pai <buzypi@gmail.com>
2019-01-17 16:31:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello from Singapore!!!'))
+app.get('/', (req, res) => res.send('Hello!!!'))
 
 app.get('/anotherhello', (req, res) => res.send('Another Hello!!'))
 
