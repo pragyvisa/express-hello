2bf974cc6387b3d3ae6868daa261a7d486a08005
Gautham Pai <buzypi@gmail.com>
2019-01-24 15:49:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello - from Webhook!!!'))
+app.get('/', (req, res) => res.send('Hello, today is Jan 24th!!!'))
 
 app.get('/anotherhello', (req, res) => res.send('Another Hello!!'))
 
